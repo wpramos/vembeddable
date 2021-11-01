771d9d48b401a27f35ce813c2828fd084f13e88b
Amos P <amosp323@gmail.com>
2021-11-01 16:43:06 +0530

    Add template 'search.html' for 'search' view
    - Use render() to generate HttpResponse
    
    - Also, include 'vembeddable' AppConfig to INSTALLED_APPS to make app templates discoverable to Django

--- src/vembeddable/views.py
+++ src/vembeddable/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 # Create your views here.
 
 
 def search(request):
-    return HttpResponse('view_search')
+    return render(request, 'vembeddable/search.html')
