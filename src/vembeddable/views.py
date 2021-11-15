7bb175bfcd59b9961673186a806d6770fd495949
Amos P <amosp323@gmail.com>
2021-11-15 14:45:16 +0530

    Update 'search' view to get YouTube search response using 'googleapiclient' for Python
    - Update requirements.txt

--- src/vembeddable/views.py
+++ src/vembeddable/views.py
@@ -1,7 +1,37 @@
+import os
+
 from django.shortcuts import render
+
+from googleapiclient.discovery import build
 
 # Create your views here.
 
 
 def search(request):
-    return render(request, 'vembeddable/search.html')
+    # GET API KEY
+    youtube_api_key = os.environ['VEMBEDDABLE_YOUTUBE_API_KEY']
+
+    # BUILD YouTube API SERVICE OBJECT
+    youtube = build('youtube', 'v3', developerKey=youtube_api_key)
+
+    # CREATE HTTP REQUEST OBJECT AND EXECUTE REQUEST
+    query = 'user input'
+
+    youtube_api_request = youtube.search().list(
+        part = 'snippet',
+        q = query,
+        # relevanceLanguage = 'en',
+        type = 'video',
+        videoEmbeddable = 'true',
+        videoSyndicated = 'true',
+        videoCaption = 'closedCaption',
+        maxResults = '50',
+    )
+
+    youtube_api_response = youtube_api_request.execute()
+
+    context = {
+        'youtube_api_response': youtube_api_response,
+    }
+
+    return render(request, 'vembeddable/search.html', context)
